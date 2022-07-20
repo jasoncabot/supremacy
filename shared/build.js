d972d968ed49eb52b3ae6b214c6ab3a947cb5c97
Jason Cabot <jasoncabot@gmail.com>
2022-07-20 21:23:31 +0100

    Added vscode debugging to frontend with live reloading and shared support

--- shared/build.js
+++ shared/build.js
@@ -1,6 +1,8 @@
 const esbuild = require('esbuild');
 const path = require('path');
 
+console.log('[esbuild] start');
+
 esbuild.build({
     entryPoints: [path.join(__dirname, "src", "index.ts")],
     outfile: path.join(__dirname, "dist", "shared.js"),
@@ -15,3 +17,4 @@ esbuild.build({
     process.exitCode = 1
 }
 );
+console.log('[esbuild] finish');
